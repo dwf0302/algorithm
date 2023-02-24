b191458c045dfc8f4a34835720c688d3bd9f042c
duweifeng <>
2023-02-24 11:27:02 +0800

    [feature]快排

--- src/main/java/cn/weifeng/sort/QuickSort.java
+++ src/main/java/cn/weifeng/sort/QuickSort.java
@@ -6,38 +6,46 @@ package cn.weifeng.sort;
 public class QuickSort {
     public static void main(String[] args) {
 
-        int[] arr = {4, 1, 2, 4, 3, 5, 0, 3, 7, 1, 9};
+        int[] arr = {4, 1, 2, 4, 3, 5, 3};
 
-        partition(arr, 3);
+        // int[] partition = partition(arr, -1, arr.length - 1);
 
+        sort(arr, 0, arr.length - 1);
         System.out.println("");
 
+    }
+
+
+    public static void sort(int[] arr, int l, int r) {
+        if (l < r) {
+            // 让数更加随机 可改进快排效率
+            int[] p = partition(arr, l, r);
+            sort(arr, l, p[0] - 1);
+            sort(arr, p[1] + 1, r);
+        }
     }
 
     /**
      * 荷兰国旗问题
      *
      * @param arr
-     * @param num
+     * @param l
+     * @param r
      */
-    public static void partition(int[] arr, int num) {
-
-        int l = -1;
-
-        int r = arr.length;
-
-        int i = 0;
-
-        while (i < r) {
-            if (arr[i] < num) {
-                swap(arr, ++l, i++);
-            } else if (arr[i] > num) {
-                swap(arr, --r, i);
+    public static int[] partition(int[] arr, int l, int r) {
+        int L = l - 1;
+        int R = r;
+        while (l < R) {
+            if (arr[l] > arr[r]) {
+                swap(arr, l, --R);
+            } else if (arr[l] < arr[r]) {
+                swap(arr, ++L, l++);
             } else {
-                i++;
+                l++;
             }
         }
-
+        swap(arr, r, R);
+        return new int[]{L + 1, R};
     }
 
     public static void swap(int[] arr, int i, int j) {
